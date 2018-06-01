70880209d21dc5f705833ef577a8ffbecb690068
takubo11 <takubo@talknote.com>
2018-06-01 18:42:39 +0900

    集計画面からリンクを追加

--- src/html/aggregate.php
+++ src/html/aggregate.php
@@ -2,14 +2,54 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>集計</title>
+	<script type="text/javascript">
 
+		//イベント処理を追加
+		window.onload = function(){
+			alert("onloadが呼び出されました");
+			aggregate_label = document.getElementById("aggregate");
+			aggregate_label.onclick = fnc1;
+
+			income_label = document.getElementById("income");
+			income_label.onclick = submitForm;
+
+			expence_label = document.getElementById("expence");
+			expence_label.onclick = submitForm;
+
+		}
+
+		function fnc1(){ alert("関数１が呼び出されました"); }
+
+		function submitForm( value ){
+			var form = document.createElement( ‘form’ );
+			document.body.appendChild( form );
+			var input = document.createElement( ‘input’ );
+			input.setAttribute( ‘type’ , ‘hidden’ );
+			input.setAttribute( ‘name’ , ‘name’ );
+			input.setAttribute( ‘value’ , value );
+			form.appendChild( input );
+			form.setAttribute( ‘action’ , ‘income.php’ );
+			form.setAttribute( ‘method’ , ‘get’ );
+			form.submit();
+		}
+	</script>
+    <title>集計表示</title>
 </head>
 <body>
-<?php echo 'phpからHello World!'?>
-	<form action="http://localhost:8080/aggregate.php">
-		<label for="say">What greeting do you want to say?</label>
-		<input name="say" id="say" value="Hi">
-	</form>
+	<span id="aggregate">
+		集計表示
+	</span>
+	&nbsp;
+
+	<span id="income">
+		収入入力
+	</span>
+	&nbsp;
+
+	<span id="expence">
+		支出入力
+	</span>
+	&nbsp;
+
 </body>
 </html>
