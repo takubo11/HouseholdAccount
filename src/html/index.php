acdbf490fc954a57445d54005f8270b07a5e132d
takubo11 <takubo@talknote.com>
2018-06-01 17:14:25 +0900

    add table

--- src/html/index.php
+++ src/html/index.php
@@ -8,8 +8,6 @@ $pdo = new PDO('mysql:host=talknote.dev;dbname=household;charset=utf8mb4',
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 	]);
-//$stmt = $pdo->query('SELECT hoge FROM hoge');
-//$hoge = $pdo->exec('SELECT hoge FROM hoge');
 $hoge = $pdo->exec('update hoge set hoge = 3');
 
 print_r($hoge);
---
+++ src/schema/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `expence` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `expence_name` int(11) NOT NULL,
+ PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE `income` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `income_name` int(11) NOT NULL,
+ PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE `accounts` (
+ `id` bigint NOT NULL AUTO_INCREMENT,
+ `account_type` int(1) NOT NULL,
+ `item_name` varchar(1000) NOT NULL ,
+ `account_id` int(11) NOT NULL,
+ `amount` bigint,
+ `created_time` timestamp NOT NULL,
+ PRIMARY KEY (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
