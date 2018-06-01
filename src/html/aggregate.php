324acaafd22c6b3106bda300af41af6dd5e0c82f
takubo11 <takubo@talknote.com>
2018-06-01 17:22:29 +0900

    集計画面を追加

---
+++ src/html/aggregate.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World!</title>
+    <?php echo 'phpからHello World!'?>
+</head>
+<body>
+Hello World!
+</body>
+</html>
